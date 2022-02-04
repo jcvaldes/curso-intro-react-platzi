055cf676bb751e4dc698f8679ff7e9ad3e613aae
Juan Carlos Valdes <idevkingos@gmail.com>
2022-02-04 10:41:31 -0300

    explicacion de eventos

--- 02-todo/src/components/CreateTodoButton.js
+++ 02-todo/src/components/CreateTodoButton.js
@@ -2,7 +2,48 @@ import React from "react";
 import "./CreateTodoButton.css";
 
 function CreateTodoButton(props) {
-  return <button className="CreateTodoButton">+</button>;
+  // const onClickButton = () => {
+  //   alert("Create Todo Button Clicked");
+  // };
+  // const onClickButton = (msg) => {
+  //   alert(msg);
+  // };
+  const onClickButton = (msg) => {
+    return () => {
+      alert(msg);
+    };
+  };
+  return (
+    // <button className="CreateTodoButton" onClick={alert("hola mundo")}>
+    //   +
+    // </button>
+    // <button className="CreateTodoButton" onClick={() => alert("hola mundo")}>
+    //   +
+    // </button>
+
+    // <button className="CreateTodoButton" onClick={onClickButton}>
+    //   +
+    // </button>
+    // <button
+    //   className="CreateTodoButton"
+    //   onClick={onClickButton("Create Todo Button Clicked")}
+    // >
+    //   +
+    // </button>
+    // <button
+    //   className="CreateTodoButton"
+    //   onClick={() => onClickButton("Create Todo Button Clicked")}
+    // >
+    //   +
+    // </button>
+
+    <button
+      className="CreateTodoButton"
+      onClick={onClickButton("Create Todo Button Clicked")}
+    >
+      +
+    </button>
+  );
 }
 
 export { CreateTodoButton };
--- 02-todo/src/components/TodoItem.js
+++ 02-todo/src/components/TodoItem.js
@@ -13,17 +13,26 @@ import "./TodoItem.css";
 // export { TodoItem };
 
 function TodoItem(props) {
+  const onComplete = () => {
+    alert("Ya completaste la tarea " + props.text);
+  };
+  const onDelete = () => {
+    alert("Borraste la tarea " + props.text);
+  };
   return (
     <li className="TodoItem">
       <span
         className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={onComplete}
       >
         √
       </span>
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">X</span>
+      <span className="Icon Icon-delete" onClick={onDelete}>
+        X
+      </span>
     </li>
   );
 }
