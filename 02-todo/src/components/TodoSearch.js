a0fc30e2ff1c23377e202700700e04805000f82e
Juan Carlos Valdes <idevkingos@gmail.com>
2022-02-04 11:02:28 -0300

    explicacion de manejo de estados

--- 02-todo/src/components/TodoSearch.js
+++ 02-todo/src/components/TodoSearch.js
@@ -2,16 +2,45 @@ import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState("");
+
   const onSearchValueChange = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
+    setSearchValue(event.target.value);
   };
-  return (
+  // return (
+  //   <>
+  //     <input
+  //       className="TodoSearch"
+  //       placeholder="Cebolla"
+  //       value={searchValue}
+  //       onChange={onSearchValueChange}
+  //     />
+  //     <p>{searchValue}</p>
+  //   </>
+  // );
+  // Uso un array en vez de fragment
+  return [
     <input
       className="TodoSearch"
       placeholder="Cebolla"
+      value={searchValue}
       onChange={onSearchValueChange}
-    />
-  );
+    />,
+    <p>{searchValue}</p>,
+  ];
 }
 
 export { TodoSearch };
+
+// Antes el state se escribía así:
+// class Componente extends React.Component {
+//   constructor() {
+//     this.state = {
+//       name: "Juan",
+//     };
+//   }
+//   render() {
+//     return <div onClick={() => this.setState("Oliver")}>{this.state.name}</div>;
+//   }
+// }
